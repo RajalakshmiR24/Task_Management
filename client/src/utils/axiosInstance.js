def576b9c5f4c32e12813aee554a9b8bfe840401
RAJA LAKSHMI R <127002476+RajalakshmiR24@users.noreply.github.com>
2025-04-27 15:03:06 +0530

    Update axiosInstance.js

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://task-management-ywjn.vercel.app",
+const baseURL = process.env.REACT_APP_API_URL || window.location.origin || "https://task-management-ywjn.vercel.app";
   headers: {
     "Content-Type": "application/json",
   },
